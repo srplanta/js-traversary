f140d0aa09029790131e8d647ada30f7e561407c
srplanta <srplanta@gmail.com>
2023-09-22 08:19:35 +0500

    if else, swith statement and arrow function added

--- script.js
+++ script.js
@@ -1,70 +1,147 @@
-let person = {
-    personName: "Aleem Khan",
-    age: 30,
-    hobbies: "music, movies, sports",
-    address: {
-        country: "Pakistan",
-        province: "Punjab",
-        city: "Rawalpindi"
-    },
-    isMarried: false
+// let person = {
+//     personName: "Aleem Khan",
+//     age: 30,
+//     hobbies: "music, movies, sports",
+//     address: {
+//         country: "Pakistan",
+//         province: "Punjab",
+//         city: "Rawalpindi"
+//     },
+//     isMarried: false
+// }
+// //add a property in object
+// person.email = "person@gmail.com";
+
+// document.write(`Person Name: ${person.personName} <br>`);
+// document.write(`Person Age: ${person.age} <br>`);
+// document.write(`Person Hobbies: ${person.hobbies} <br>`);
+// document.write(`Person Country: ${person.address.country} <br>`);
+// document.write(`Marital Status: ${person.isMarried} <br>`);
+// document.write(`Added property: ${person.email} <br>`);
+// document.write("<hr><br>");
+// // ______________________________________________________________
+// //declare array object literal
+// const todos = [
+//     {
+//         id: 1,
+//         task: "Take out trash",
+//         isCompleted: true
+//     },
+//     {
+//         id: 2,
+//         task: "Meeting with boss",
+//         isCompleted: true
+//     },
+//     {
+//         id: 3,
+//         task: "Visit doctor",
+//         isCompleted: false
+//     }
+// ];
+
+// // ******************************
+// //      TRAVERSING THE ARRAY
+// // ******************************
+// //Method-1 : for Loop
+// document.write("ARRAY TRAVERSAL BY SIMPLE (for) LOOP<BR>");
+// document.write("-------------------------------");
+// document.write("-------------------------------<BR>");
+// document.write("COMPLETED TASKS<BR>");
+// document.write("------------------------------<BR>");
+// for (let i = 0; i < todos.length; i++) {
+//     if (todos[i].isCompleted) {
+//         document.write(`ID: ${todos[i].id} <br>`);
+//         document.write(`TASK: ${todos[i].task} <br>`);
+//         document.write(`STATUS: ${todos[i].isCompleted} <br>`);
+//         document.write("<br>")
+//     }
+// }
+// document.write("<hr><br>");
+
+// // ______________________________________________________________
+// //Method-1 : forEach Loop
+// document.write("ARRAY TRAVERSAL BY (forEach) LOOP<BR>");
+// document.write("-------------------------------");
+// document.write("-------------------------------<BR>");
+// document.write("COMPLETED TASKS<BR>");
+// document.write("------------------------------<BR>");
+// todos.forEach(element => {
+//     if (element.isCompleted) {
+//         document.write(`ID: ${element.id} <br>`);
+//         document.write(`TASK: ${element.task} <br>`);
+//         document.write(`STATUS: ${element.isCompleted} <br>`);
+//         document.write("<br>")
+//     }
+// });
+// document.write("<hr><br>");
+// // ______________________________________________________________
+
+// //extract new array from todos array consisting of only todos.task
+// let todosTasks = todos.map(function (todo) {
+//     return todo.task;
+// })
+// document.write("Todos completed taskes extracted in new array  from todos with (map function).<br>");
+// document.write("---------------------------------");
+// document.write("---------------------------------<br>");
+// document.write(`Tasks array from todosTasks array:<br> ${todosTasks} <br>`);
+// todosTasks.forEach(element => {
+//     document.write(`Task: ${element}<br>`);
+// })
+// document.write("<hr><br>")
+// // ______________________________________________________________
+
+// //Extracting todos completed tasks by applying filter
+// // let completedTasks = todos.filter(todo => {
+// //     return todo.isCompleted;
+// // })
+// let completedTasks = todos.filter(todo => todo.isCompleted);
+// document.write(`data type of completed tasks: ${completedTasks}<br>`);
+// completedTasks.forEach(task => {
+//     document.write(`id: ${task.id}<br>`);
+//     document.write(`task: ${task.task}<br>`);
+//     document.write(`status: ${task.isCompleted}<br>`);
+// });
+// // ______________________________________________________________
+
+// //to send/recieve data to/from server
+// let todosJSON = JSON.stringify(todos);
+// document.write(todosJSON);
+// // ______________________________________________________________
+
+// ***************************************************************
+//                  SIMPLE PROGRAMMING CONSTRUCTS
+// ***************************************************************
+let x = 10;
+if (x > 10) {
+    document.write("x > 10<br>");
+} else if (x < 10) {
+    document.write("x < 10<br>");
+} else {
+    document.write("x = 10<br>");
 }
-//add a property in object
-person.email = "person@gmail.com";
-
-document.write(`Person Name: ${person.personName} <br>`);
-document.write(`Person Age: ${person.age} <br>`);
-document.write(`Person Hobbies: ${person.hobbies} <br>`);
-document.write(`Person Country: ${person.address.country} <br>`);
-document.write(`Marital Status: ${person.isMarried} <br>`);
-document.write(`Added property: ${person.email} <br>`);
 document.write("<hr><br>");
-// ______________________________________________________________
 
-const todos = [
-    {
-        id: 1,
-        task: "Take out trash",
-        isCompleted: true
-    },
-    {
-        id: 2,
-        task: "Meeting with boss",
-        isCompleted: true
-    },
-    {
-        id: 3,
-        task: "Visit doctor",
-        isCompleted: false
-    }
-];
+// --------------- SWITCH STATEMENT ---------------
+let color = x >= 10 ? "red" : "green";
+switch (color) {
+    case "red":
+        document.write("color is RED<br>");
+        break;
 
-document.write("COMPLETED TASKS<BR>");
-document.write("------------------------------<BR>");
-todos.forEach(element => {
-    if (element.isCompleted) {
-        document.write(`ID: ${element.id} <br>`);
-        document.write(`TASK: ${element.task} <br>`);
-        document.write(`STATUS: ${element.isCompleted} <br>`);
-        document.write("<br>")
-    }
-});
-document.write("<hr><br>")
-// ______________________________________________________________
+    case "green":
+        document.write("color is GREEN<br>");
+        break;
 
-//new array from todos array consisting only on todos.task
-let todosTasks = todos.map(function (todo) {
-    return todo.task;
-})
-document.write(`Tasks array from todos:<br> ${todosTasks} <br>`);
-todosTasks.forEach(element => {
-    document.write(`Task: ${element}<br>`);
-})
-document.write("<hr><br>")
-// ______________________________________________________________
+    default:
+        document.write("color is neither RED nor GREEN<br>");
+        break;
+}
+document.write("<hr><br>");
 
-//to send/recieve data to/from server
-let todosJSON = JSON.stringify(todos);
-document.write(todosJSON);
-// ______________________________________________________________
+// --------------- FUNCTIONS ---------------
+const AddNums = (num1 = 0, num2 = 0) => {
+    return num1 + num2;
+}
 
+document.write(AddNums(12, 23));
+document.write("<hr><br>");
